3d192c13ebb9ac028914258ca930755db7bbfcb2
Guilherme <g.vinicius@muvr.io>
2024-09-03 21:33:07 -0300

    ffirst commit

---
+++ composeApp/src/commonMain/kotlin/dev/dartic/tvmaze/Platform.kt
@@ -0,0 +1,7 @@
+package dev.dartic.tvmaze
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
---
+++ composeApp/src/iosMain/kotlin/dev/dartic/tvmaze/MainViewController.kt
@@ -0,0 +1,5 @@
+package dev.dartic.tvmaze
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
